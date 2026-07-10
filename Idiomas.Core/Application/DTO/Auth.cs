33960fc3e0f4a8b31a111220c91f9922c1ebe1ec
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2026-07-10 19:34:28 -0300

    feat: add ForgotPasswordDTO and ResetPasswordDTO

--- Idiomas.Core/Application/DTO/Auth.cs
+++ Idiomas.Core/Application/DTO/Auth.cs
@@ -1,3 +1,7 @@
 namespace Idiomas.Core.Application.DTO.Auth;
 
 public record MailPasswordLoginDTO(string Email, string Password);
+
+public record ForgotPasswordDTO(string Email);
+
+public record ResetPasswordDTO(string Token, string NewPassword);
