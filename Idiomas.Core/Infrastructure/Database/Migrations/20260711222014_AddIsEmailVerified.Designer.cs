6cb1ca9618c30e79d5da724fe21c4364c9b59f52
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2026-07-11 19:21:30 -0300

    feat: add migration for IsEmailVerified column with existing users as verified

--- Idiomas.Core/Infrastructure/Database/Migrations/20260711222014_AddIsEmailVerified.Designer.cs
+++ Idiomas.Core/Infrastructure/Database/Migrations/20260711222014_AddIsEmailVerified.Designer.cs
@@ -4,6 +4,7 @@ using Idiomas.Core.Infrastructure.Database.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Idiomas.Core.Infrastructure.Database.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20260711222014_AddIsEmailVerified")]
+    partial class AddIsEmailVerified
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Idiomas.Core/Infrastructure/Database/Migrations/20260711222014_AddIsEmailVerified.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Idiomas.Core.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsEmailVerified : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_email_verified",
+                table: "user",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.Sql("UPDATE [user] SET [is_email_verified] = 1 WHERE [is_email_verified] = 0;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_email_verified",
+                table: "user");
+        }
+    }
+}
